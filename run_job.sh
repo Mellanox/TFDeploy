94c6b1e41928f6b2feb92f568b8b6b6c6305b6d3
Elad Weiss <eladw@ezchip.com>
2017-09-07 16:32:30 +0300

    Add color to meta print on run_job.sh
    Signed-off-by: Elad Weiss <eladw@ezchip.com>

--- run_job.sh
+++ run_job.sh
@@ -12,16 +12,17 @@ worker_hosts=$4
 title="[$(dirname $0)]: `hostname`  - $job_name:$task_id"
 echo -ne "\033]0;$title\007"
 
+echo -e "\033[1;32m"
 echo "Running `hostname`:"
 echo "   + Job: $job_name" 
 echo "   + Task ID: $task_id"
 echo "   + PS hosts: $ps_hosts"
 echo "   + Worker hosts: $worker_hosts"
 echo
+echo -e "\033[0;0m"
 
 mkdir -p run_logs
 python -u $script_dir/tf_cnn_benchmarks.py $job_name $task_id $ps_hosts $worker_hosts 2>&1 | tee $script_dir/run_logs/${job_name}_${task_id}.log
-process_id=$!
 
 if [[ ($job_name == worker) && ($task_id -eq 0) ]]; then
 	touch done.txt
