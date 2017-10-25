c92f9532f41aa3bce60d2fcd31b3d314ece61b3f
Elad Weiss <eladw@ezchip.com>
2017-10-25 10:04:16 +0300

    MASTER: Build with --config=cuda.
    Signed-off-by: Elad Weiss <eladw@ezchip.com>

--- deploy.sh
+++ deploy.sh
@@ -168,7 +168,7 @@ then
 	echo "   See: $logs_dir/build.log"
 	[[ -z $TENSORFLOW_HOME ]] && TENSORFLOW_HOME=/root/tensorflow/
 	cd $TENSORFLOW_HOME
-	bazel build --config=opt $TENSORFLOW_BUILD_FLAGS //tensorflow/tools/pip_package:build_pip_package >& $logs_dir/build.log &
+	bazel build --config=cuda $TENSORFLOW_BUILD_FLAGS //tensorflow/tools/pip_package:build_pip_package >& $logs_dir/build.log &
 	build_pid=$!
 	echo "   PID: $build_pid"
 	echo -n "   Progress: "
